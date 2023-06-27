d989d3ed9b21a162e1708e2defb104944e18fd2d
Clovis Pilger Junior <clovispilgerjunior@hotmail.com>
2023-06-27 17:42:35 -0300

    Updated some class

--- src/RavinMain.java
+++ src/RavinMain.java
@@ -1,8 +1,15 @@
+import controller.UsuarioController;
+import view.UsuarioView;
+
 import javax.swing.*;
 import java.awt.*;
 
 public class RavinMain {
     public static void main(String[] args) {
+        UsuarioController usuarioController = new UsuarioController();
 
+        UsuarioView usuarioView = new UsuarioView(usuarioController);
+
+        usuarioView.loginUsuarioPanel();
     }
 }
--- src/view/UsuarioView.java
+++ src/view/UsuarioView.java
@@ -1,9 +1,62 @@
 package view;
 
+import controller.UsuarioController;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UsuarioView {
+  private UsuarioController usuarioController;
+
+  public UsuarioView(UsuarioController usuarioController) {
+    this.usuarioController = usuarioController;
+  }
+
+  public void loginUsuarioPanel() {
+    JFrame loginFrame = new JFrame("Tela de Login");
+    loginFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    JPanel panel = new JPanel(new GridLayout(3, 2));
+
+    JLabel usuarioLabel = new JLabel("Usuário:");
+    JTextField usuarioField = new JTextField(10);
+
+    JLabel senhaLabel = new JLabel("Senha:");
+    JPasswordField senhaField = new JPasswordField(10);
+
+    JButton loginButton = new JButton("Login");
+
+    loginButton.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        String usuario = usuarioField.getText();
+        char[] senhaChars = senhaField.getPassword();
+        String senha = new String(senhaChars);
+
+        // Lógica de autenticação do usuário aqui
+
+        // Exemplo simples: exibir mensagem com os dados digitados
+        JOptionPane.showMessageDialog(loginFrame, "Usuário: " + usuario + "\nSenha: " + senha);
+
+        // Após a autenticação, você pode realizar outras ações, como mostrar a tela principal do sistema
+        // ou chamar outros métodos do controlador, dependendo da sua lógica de negócio.
+      }
+    });
+
+    panel.add(usuarioLabel);
+    panel.add(usuarioField);
+    panel.add(senhaLabel);
+    panel.add(senhaField);
+    panel.add(new JLabel()); // Espaço vazio para alinhar o botão
+    panel.add(loginButton);
+
+    loginFrame.add(panel);
+    loginFrame.pack();
+    loginFrame.setLocationRelativeTo(null); // Centralizar a janela na tela
+    loginFrame.setVisible(true);
+  }
   public void cadastrarUsuarioPanel() {
     JTextField idCampo = new JTextField(10);
     JTextField loginCampo = new JTextField(10);
@@ -31,5 +84,6 @@ public class UsuarioView {
       String tipoUsuario = tipoUsuarioCampo.getText();
       String ativo = ativoCampo.getText();
     }
+
   }
 }
